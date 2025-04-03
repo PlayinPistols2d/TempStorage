07a8ec824fc4c934caa7aa67e632fdd6e9246e61
Artem(Artyom) <77617130+PlayinPistols2d@users.noreply.github.com>
2025-04-03 10:46:56 +0300

    Update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -3,74 +3,52 @@
 
 #include <QString>
 
-class BitConverter
-{
+class BitConverter {
 public:
-    static QString hexToBinary(const QString &hex);
-    static QString binaryToHex(const QString &typeBin, const QString &kindBin);
+    static std::pair<int, int> hexToTypeKind(const QString& hex);
+    static QString typeKindToHex(int type, int kind);
 };
 
 #endif // BITCONVERTER_H
 
 
 
+#include "BitConverter.h"
 
-#include "bitconverter.h"
-
-QString BitConverter::hexToBinary(const QString &hex)
-{
+std::pair<int, int> BitConverter::hexToTypeKind(const QString& hex) {
     bool ok;
-    uint16_t value = hex.toUShort(&ok, 16);
+    int value = hex.toInt(&ok, 16);
+    if (!ok) return {-1, -1};  // Ошибка: некорректный ввод
 
-    if (!ok || value > 0xFFFF) {
-        return "Ошибка: Некорректный HEX";
-    }
+    int type = (value >> 6) & 0x3FF; // 10 старших бит
+    int kind = value & 0x3F;         // 6 младших бит
 
-    uint16_t type = (value >> 6) & 0x3FF; // Старшие 10 бит
-    uint16_t kind = value & 0x3F;        // Младшие 6 бит
-
-    return QString("Тип: %1 (0b%2)\nВид: %3 (0b%4)")
-        .arg(type)
-        .arg(QString::number(type, 2).rightJustified(10, '0'))
-        .arg(kind)
-        .arg(QString::number(kind, 2).rightJustified(6, '0'));
+    return {type, kind};
 }
 
-QString BitConverter::binaryToHex(const QString &typeBin, const QString &kindBin)
-{
-    bool okType, okKind;
-    uint16_t type = typeBin.toUShort(&okType, 2);
-    uint16_t kind = kindBin.toUShort(&okKind, 2);
+QString BitConverter::typeKindToHex(int type, int kind) {
+    if (type < 0 || type > 1023 || kind < 0 || kind > 63)
+        return "Ошибка: Выход за пределы";
 
-    if (!okType || !okKind || type > 0x3FF || kind > 0x3F) {
-        return "Ошибка: Некорректный ввод";
-    }
-
-    uint16_t result = (type << 6) | kind;
-    return QString("HEX: 0x%1").arg(result, 4, 16, QChar('0')).toUpper();
+    int value = (type << 6) | kind;
+    return QString::number(value, 16).toUpper().rightJustified(4, '0');
 }
-
-
-
 
 
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QClipboard>
-#include "bitconverter.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private slots:
@@ -85,50 +63,51 @@ private:
 
 
 
-
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+#include "BitConverter.h"
+#include <QClipboard>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow)
-{
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
-
-    ui->hexInput->setPlaceholderText("Введите HEX (0xFFFF)");
-    ui->typeInput->setPlaceholderText("Введите 10-битное число (бинарно)");
-    ui->kindInput->setPlaceholderText("Введите 6-битное число (бинарно)");
-
-    connect(ui->convertButton, &QPushButton::clicked, this, &MainWindow::on_convertButton_clicked);
-    connect(ui->copyButton, &QPushButton::clicked, this, &MainWindow::on_copyButton_clicked);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
-void MainWindow::on_convertButton_clicked()
-{
-    QString output;
-    if (!ui->hexInput->text().isEmpty()) {
-        output = BitConverter::hexToBinary(ui->hexInput->text());
-    } else if (!ui->typeInput->text().isEmpty() && !ui->kindInput->text().isEmpty()) {
-        output = BitConverter::binaryToHex(ui->typeInput->text(), ui->kindInput->text());
-    } else {
-        output = "Ошибка: Введите данные";
+void MainWindow::on_convertButton_clicked() {
+    if (ui->hexInput->text().isEmpty()) {  // Если вводится два числа
+        bool ok1, ok2;
+        int type = ui->typeInput->text().toInt(&ok1, 2); // Двоичный ввод
+        int kind = ui->kindInput->text().toInt(&ok2, 2);
+
+        if (!ok1 || !ok2 || type > 1023 || kind > 63) {
+            QMessageBox::warning(this, "Ошибка", "Некорректный ввод чисел");
+            return;
+        }
+
+        QString hexResult = BitConverter::typeKindToHex(type, kind);
+        ui->resultLabel->setText("HEX: " + hexResult);
+    } else {  // Если вводится HEX
+        auto [type, kind] = BitConverter::hexToTypeKind(ui->hexInput->text());
+
+        if (type == -1) {
+            QMessageBox::warning(this, "Ошибка", "Некорректный HEX-ввод");
+            return;
+        }
+
+        ui->resultLabel->setText("Тип: " + QString::number(type, 2) + " | Вид: " + QString::number(kind, 2));
     }
-    
-    ui->outputLabel->setText(output);
 }
 
-void MainWindow::on_copyButton_clicked()
-{
+void MainWindow::on_copyButton_clicked() {
     QClipboard *clipboard = QApplication::clipboard();
-    clipboard->setText(ui->outputLabel->text());
+    clipboard->setText(ui->resultLabel->text());
+    QMessageBox::information(this, "Скопировано", "Результат скопирован в буфер обмена");
 }
-
-
-
 
 
 
@@ -138,92 +117,44 @@ void MainWindow::on_copyButton_clicked()
  <widget class="QMainWindow" name="MainWindow">
   <property name="geometry">
    <rect>
-    <x>0</x>
-    <y>0</y>
+    <x>100</x>
+    <y>100</y>
     <width>400</width>
     <height>300</height>
    </rect>
   </property>
-  <property name="windowTitle">
-   <string>Bit Converter</string>
-  </property>
   <widget class="QWidget" name="centralwidget">
    <widget class="QLineEdit" name="hexInput">
-    <property name="geometry">
-     <rect>
-      <x>20</x>
-      <y>20</y>
-      <width>360</width>
-      <height>30</height>
-     </rect>
+    <property name="placeholderText">
+     <string>Введите HEX (4 знака)</string>
     </property>
    </widget>
    <widget class="QLineEdit" name="typeInput">
-    <property name="geometry">
-     <rect>
-      <x>20</x>
-      <y>60</y>
-      <width>175</width>
-      <height>30</height>
-     </rect>
+    <property name="placeholderText">
+     <string>Введите тип (10 бит)</string>
     </property>
    </widget>
    <widget class="QLineEdit" name="kindInput">
-    <property name="geometry">
-     <rect>
-      <x>205</x>
-      <y>60</y>
-      <width>175</width>
-      <height>30</height>
-     </rect>
+    <property name="placeholderText">
+     <string>Введите вид (6 бит)</string>
     </property>
    </widget>
    <widget class="QPushButton" name="convertButton">
-    <property name="geometry">
-     <rect>
-      <x>20</x>
-      <y>100</y>
-      <width>360</width>
-      <height>30</height>
-     </rect>
-    </property>
     <property name="text">
      <string>Конвертировать</string>
     </property>
    </widget>
-   <widget class="QLabel" name="outputLabel">
-    <property name="geometry">
-     <rect>
-      <x>20</x>
-      <y>140</y>
-      <width>360</width>
-      <height>60</height>
-     </rect>
+   <widget class="QPushButton" name="copyButton">
+    <property name="text">
+     <string>Скопировать</string>
     </property>
+   </widget>
+   <widget class="QLabel" name="resultLabel">
     <property name="text">
      <string>Результат</string>
     </property>
-    <property name="alignment">
-     <set>Qt::AlignCenter</set>
-    </property>
-   </widget>
-   <widget class="QPushButton" name="copyButton">
-    <property name="geometry">
-     <rect>
-      <x>20</x>
-      <y>210</y>
-      <width>360</width>
-      <height>30</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>Копировать</string>
-    </property>
    </widget>
   </widget>
-  <layout class="QVBoxLayout" name="verticalLayout"/>
-  <widget class="QMenuBar" name="menubar"/>
-  <widget class="QStatusBar" name="statusbar"/>
  </widget>
  <resources/>
  <connections/>
